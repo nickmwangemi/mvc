b4dc5b4549ed6bc13dd3ada7f556fdac158a8945
nickmwangemi <nickmwangemi@gmail.com>
2020-12-17 23:17:18 +0300

    Add helper methods for DOM manipulation

--- script.js
+++ script.js
@@ -39,6 +39,18 @@ class Model {
 
 class View {
 	constructor() {}
+
+	createElement(tag, className) {
+		const element = document.createElement(tag)
+		if (className) element.classList.add(className)
+
+		return element
+	}
+
+	getElement(selector) {
+		const element = document.querySelector(selector)
+		return element
+	}
 }
 
 class Controller {
